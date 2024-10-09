a3197da4bb479be3760246bd91e17ccd0c502033
ghanshyam shukla <gsgmca682@gmail.com>
2024-10-09 08:52:56 +0530

    initial commit

---
+++ src/main/java/com/tutorial/ansible/DockerAnsibleDemoApp.java
@@ -0,0 +1,13 @@
+package com.tutorial.ansible;
+
+/**
+ * Hello world!
+ *
+ */
+public class DockerAnsibleDemoApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
